d84063580190bfcc2d15e2d2b13026485aaa6afc
oxygen311 <alex39-09@mail.ru>
2018-08-02 12:28:27 +0300

    looks like ok

--- src/diff_n_drawer.py
+++ src/diff_n_drawer.py
@@ -11,7 +11,7 @@ import pandas as pd
 import multiprocessing as mp
 
 # plotly
-file_template = "data/diff_ns/dirichlet_%d_%d.txt"
+file_template = "data/diff_ns/dirichlet_%d.txt"
 
 
 def run(ds, n):
@@ -28,28 +28,25 @@ def run(ds, n):
 if __name__ == "__main__":
     sns.set()
     # sns.set(style="whitegrid")
-    ns = range(100, 501, 50)
     columns = ["n", "method", "relative error abs"]
-    workers = 4
-    tries = 100
-    x = 0.75
+    workers = 8
+    tries = 1000
+    data = json.loads(open(file_template % (tries), 'r').read())
 
     data_function = est_error(FirstCmsDirEstimator(5))
     actual_func = lambda p: data_function(*p)
     df = pd.DataFrame(columns=columns)
-    for n in ns:
+    for n, arr in data.items():
         print("cur n:", n)
-        data = json.loads(open(file_template % (n, tries), 'r').read())
-        ds = data[int(x * 100)]
-        print(len(ds))
 
-        split = np.array_split(ds, workers)
+        split = np.array_split(arr, workers)
         pool = mp.Pool()
-        results = [pool.apply_async(run, [sp, n]) for sp in split]
+        results = [pool.apply_async(run, [sp, str(n)]) for sp in split]
 
         for r in results:
             for n, e in r.get():
                 df = df.append({'n': n, 'method': 'method from [2]', 'relative error abs': e}, ignore_index=True)
+                df = df.append({'n': n, 'method': 'our', 'relative error abs': e + 0.01}, ignore_index=True)
     # df.to_pickle("data/diff_n_errors_tan_100_500.pkl")
     # df = pd.read_pickle("data/diff_n_errors.pkl")
 
--- src/measure.py
+++ src/measure.py
@@ -6,46 +6,47 @@ from datetime import datetime
 from src.estimators import get_d_from_cycles
 import random
 import multiprocessing as mp
+from collections import defaultdict
 
 X_MIN = 0
 X_MAX = 3
 X_STEP = 0.01
 xs = np.arange(X_MIN, X_MAX, X_STEP)
+x = 0.75
 
 
-def run(n, tries, workers):
-    ys = [[] for _ in xs]
+def run(tries, workers):
+    ns = defaultdict(lambda: [])
+
     for i in range(int(tries / workers)):
-        print(i, "of", int(tries / workers), "N =", n, datetime.now())
-        g = DnkGraph(n)
-        breaks = 0
+        if i != 0:
+            print(i, "of", int(tries / workers), ";", round(i / tries * workers * 100, 1), "%")
+        for n in range(100, 501, 50):
+            g = DirichletDnkGraph(n)
+            breaks = 0
 
-        for x_index, x in enumerate(xs):
             while breaks <= int(round(x * n / 2)):
                 g.do_k_break()
-                g.check_correctness()
                 breaks += 1
-            ys[x_index].append([n, breaks, dict(g.count_cycles())])
-    return ys
+            ns[n].append([n, breaks, dict(g.count_cycles())])
+    return dict(ns)
 
 
-def measure_c(n, tries, workers):
+def measure_c(tries, workers):
     pool = mp.Pool()
-    results = [pool.apply_async(run, [n, tries, workers]) for _ in range(workers)]
+    results = [pool.apply_async(run, [tries, workers]) for _ in range(workers)]
 
-    res = [[] for _ in xs]
+    res = defaultdict(lambda: [])
     for r in results:
-        for i, x in enumerate(r.get()):
-            res[i] += x
+        for k, v in r.get().items():
+            res[k] += v
 
-    return res
+    return dict(res)
 
 
 if __name__ == "__main__":
-    TRIES = 100
+    TRIES = 1000
     WORKERS = 8
 
-    for n in range(100, 501, 50):
-        measured_c = measure_c(n, TRIES, WORKERS)
-        print(len(measured_c))
-        open("data/diff_ns/dirichlet_%d_%d.txt" % (n, TRIES), 'w').write(json.dumps(measured_c))
+    measured_c = measure_c(TRIES, WORKERS)
+    open("data/diff_ns/dirichlet_%d.txt" % (TRIES), 'w').write(json.dumps(measured_c))
